271fb269824e15316e8886e76cce1ec74e33dbb7
GabrielPaliari <gabriel.paliari@gmail.com>
2018-11-05 21:39:29 -0200

    merge mechanics

--- player.gd
+++ player.gd
@@ -4,21 +4,44 @@ extends KinematicBody2D
 # move_and_slide works.
 
 # Member variables
-const MOTION_SPEED = 160 # Pixels/second
-
+const MOTION_SPEED = 300 # Pixels/second
+const TILE_PORTAL = 1
+var motionString = ""
 
 func _physics_process(delta):
 	var motion = Vector2()
 	
-	if Input.is_action_pressed("move_up"):
-		motion += Vector2(0, -1)
-	if Input.is_action_pressed("move_bottom"):
-		motion += Vector2(0, 1)
-	if Input.is_action_pressed("move_left"):
-		motion += Vector2(-1, 0)
-	if Input.is_action_pressed("move_right"):
-		motion += Vector2(1, 0)
+	if get_slide_count() > 0 || motionString == "":
+		if Input.is_action_pressed("move_up"):	
+				
+			motionString = "move_up"
+		if Input.is_action_pressed("move_bottom"):
+			motionString = "move_bottom"
+		if Input.is_action_pressed("move_left"):
+			motionString = "move_left"
+		if Input.is_action_pressed("move_right"):
+			motionString = "move_right"
 	
-	motion = motion.normalized() * MOTION_SPEED
-
+	if motionString == "move_up":
+		motion += Vector2(0, -1)
+	if motionString == "move_bottom":
+		motion += Vector2(0, 1)
+	if motionString == "move_left":
+		motion += Vector2(-1, 0)
+	if motionString == "move_right":
+		motion += Vector2(1, 0)
+		
+	
+	motion = motion.normalized() * MOTION_SPEED	
+	
 	move_and_slide(motion)
+	if get_slide_count() > 0: 
+		for i in range(get_slide_count()):
+			var coll_pos = get_slide_collision(i).position
+					
+			##var collidingWith = get_collision_pos()
+			##print("hit1")
+			var tile = get_parent().get_node("TileMap").get_cellv(get_parent().get_node("TileMap").world_to_map(coll_pos))
+			print(tile);
+			if tile == TILE_PORTAL:
+				print("hit")
